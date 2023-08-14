666ba0f44c4707d952cb2e16afc13881e22f5d16
ryuwa <ryuusuzuki243@gmail.com>
2023-08-14 17:05:49 +0900

    update: ツールバーのカスタマイズ

--- src/components/editor/MarkdownEditor.tsx
+++ src/components/editor/MarkdownEditor.tsx
@@ -1,9 +1,41 @@
+import { useMemo } from "react";
 import SimpleMdeReact from "react-simplemde-editor";
+import SimpleMDE from "easymde";
 import "easymde/dist/easymde.min.css";
+const toolbar: SimpleMDE.Options["toolbar"] = [
+  'bold',
+  'italic',
+  'quote',
+  'unordered-list',
+  'ordered-list',
+  'link',
+  'image',
+  'strikethrough',
+  'code',
+  'table',
+  'redo',
+  'heading',
+  'undo',
+  'clean-block',
+  'horizontal-rule',
+  'preview',
+  'side-by-side',
+  'fullscreen'
+];
 
 const MarkdownEditor: React.FC = () => {
+  const mdeOptions: SimpleMDE.Options = useMemo(() => {
+    return {
+      width: 'auto',
+      spellChecker: false,
+      toolbar
+    };
+  }, []);
+
   return (
-    <SimpleMdeReact id="simple-mde"/>
+    <SimpleMdeReact
+      id="simple-mde"
+      options={mdeOptions}/>
   );
 };
 
