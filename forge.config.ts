0c6c70ca673f3b08ffb15f11d04e47f0e697abb5
ryuwa <ryuusuzuki243@gmail.com>
2023-08-21 19:30:51 +0900

    update: build, publishの設定

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,4 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -12,9 +8,46 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: 'images/robo'
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "electron_publish",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+      config:{}
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      platforms: ["darwin"],
+      config: {
+        repository: {
+          owner: "ryuwa-suzuki",
+          name: "zenn-md-editor",
+        },
+      },
+    }
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
